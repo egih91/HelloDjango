58857a66eb1e8ba7e28b35f33dcb7c5b095d7f9c
egih91 <93543435+egih91@users.noreply.github.com>
2022-07-13 18:02:01 +0300

    start files

--- hello_django/hello_django/urls.py
+++ hello_django/hello_django/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),#hvhjvhhj
     path('', views.about),
     path('home/', views.home),
 
